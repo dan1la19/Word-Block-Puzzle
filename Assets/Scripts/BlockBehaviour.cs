bac5b66a099ed29373f7e6dc3d8827ee304bb8de
Ivan Rechkin <r1noffs@gmail.com>
2020-10-02 18:36:41 +0500

    поле, драг энд дроп блоков

---
+++ Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BlockBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, IDropHandler
+{
+    private Vector3 startPos;
+    private bool isDragable = true;
+    private CanvasGroup group;
+
+    private void Start()
+    {
+        @group = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPos = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(!isDragable) return;
+        @group.blocksRaycasts = false;
+        var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(targetPos.x, targetPos.y, startPos.z);
+        transform.position = transform.position;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        transform.position = startPos;
+        @group.blocksRaycasts = true;
+    }
+
+    public void Place(CellBehaviour cell)
+    {
+        transform.position = cell.transform.position;
+        isDragable = false;
+    }
+}
---
+++ Assets/Scripts/CellBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CellBehaviour : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        var block = eventData.pointerDrag.GetComponent<BlockBehaviour>();
+        if (block != null)
+        {
+            block.Place(this);
+        }
+    }
+}
