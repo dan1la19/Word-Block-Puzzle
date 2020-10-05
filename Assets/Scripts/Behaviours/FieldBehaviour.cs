62f9089dabb15df77b879a3619fbfee0a6753c5c
Denis <45den450@mail.ru>
2020-10-06 04:10:07 +0500

    Блоки ставятся ровно

--- Assets/Scripts/Behaviours/FieldBehaviour.cs
+++ Assets/Scripts/Behaviours/FieldBehaviour.cs
@@ -12,15 +12,15 @@ public class FieldBehaviour : MonoBehaviour
 
     void Start()
     {
-        dist = transform.GetChild(90).position.x - transform.GetChild(91).position.x;
-        startPos = transform.GetChild(90).position;// - new Vector3(dist / 2, dist / 2);
+        dist = transform.GetChild(91).position.x - transform.GetChild(90).position.x;
+        startPos = transform.GetChild(90).position - new Vector3(dist / 2, dist / 2);
 
         fieldCells = new Dictionary<double, Dictionary<double, Transform>>();
         for (var i = 0; i < 100; i++)
         {
             var fieldCell = transform.GetChild(i);
-            var x = Math.Round(fieldCell.position.x/* - dist / 2*/, 6);
-            var y = Math.Round(fieldCell.position.y /*- dist / 2*/, 6);
+            var x = Math.Round(fieldCell.position.x - dist / 2, 6);
+            var y = Math.Round(fieldCell.position.y - dist / 2, 6);
 
             if (!fieldCells.ContainsKey(x))
             {
