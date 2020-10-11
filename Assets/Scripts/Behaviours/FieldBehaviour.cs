65623589b5b5921e87e85afa147526496282ac7e
Denis <45den450@mail.ru>
2020-10-11 20:40:34 +0500

    дебаг слов

--- Assets/Scripts/Behaviours/FieldBehaviour.cs
+++ Assets/Scripts/Behaviours/FieldBehaviour.cs
@@ -95,6 +95,10 @@ public class FieldBehaviour : MonoBehaviour
                 }
             }
 
+            var word = letters.ToString(startIndex, length);
+            if (word != "")
+                Debug.Log(word);
+
             for (var i = startIndex; i < startIndex + length; i++)
             {
                 var index = flag == 'x' ? i * 10 + c : c * 10 + i;
